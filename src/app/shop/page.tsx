52bc07da4b8f4f0d3fc7083101c41c4efee013d7
karmslajo <karms.lajo@gmail.com>
2023-04-15 03:32:40 +0800

    feat: filter packages by category

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Package } from "@prisma/client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { PackageCard } from "../../components/card/packageCard";
-import { Grid, Input } from "@nextui-org/react";
+import { Dropdown, Grid, Input, Text } from "@nextui-org/react";
 import CategoryDropdown from "../../components/dropdown/categoryDropdown";
+import { categories } from "../../types/enums";
+import { Selection } from "@react-types/shared";
 import MaterialDropdown from "../../components/dropdown/materialDropdown";
 
 async function getData() {
@@ -17,52 +19,104 @@ async function getData() {
 
 function Shop() {
   const [packages, setPackages] = useState<Package[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<Selection>(
+    new Set(["All"])
+  );
+
+  const selectedCategoryValue = useMemo(
+    () => Array.from(selectedCategory),
+    [selectedCategory]
+  );
+
+  const filteredPackages = useMemo(() => {
+    if (
+      selectedCategoryValue.length === 1 &&
+      selectedCategoryValue[0] === "All"
+    ) {
+      return packages;
+    }
+    return packages.filter((pckg) =>
+      pckg.category.some((cat) => cat == selectedCategoryValue[0])
+    );
+  }, [packages, selectedCategoryValue]);
 
   useEffect(() => {
     async function fetchData() {
-      const data = await getData();
-      setPackages(data);
+      const packages = await getData();
+      setPackages(packages);
     }
     fetchData();
   }, []);
 
   return (
-    <>
-      <div className="-mt-16">
-        <Grid.Container gap={4} justify="space-between">
-          <Grid xs={12} lg={6}>
-            <Input clearable placeholder="Search" type="search" width="100%" />
-          </Grid>
-          <Grid xs={12} lg={3}>
-            <CategoryDropdown />
-          </Grid>
-          <Grid xs={12} lg={3}>
-            <MaterialDropdown />
-          </Grid>
-        </Grid.Container>
-      </div>
-      <div>
-        <Grid.Container gap={2} justify="center">
-          {packages.map((pckg) => (
-            <Grid key={pckg.id} xs={12} sm={4} lg={3}>
-              <PackageCard
-                key={pckg.id}
-                pckg={{
-                  id: pckg.id,
-                  name: pckg.name,
-                  description: pckg.description,
-                  price: pckg.price,
-                  size: pckg.size,
-                  capacity: pckg.capacity,
-                  category: pckg.category,
-                  materials: pckg.materials,
-                }}
-              />
+      <>
+        <div className="-mt-16">
+          <Grid.Container gap={4} justify="space-between">
+            <Grid xs={12} lg={6}>
+              <Text color="gray">Search</Text>
             </Grid>
-          ))}
-        </Grid.Container>
-      </div>
-    </>
+            <Grid xs={12} lg={3}>
+              <Text color="gray" style={{ marginLeft: 3 }}>
+                Category
+              </Text>
+            </Grid>
+            <Grid xs={12} lg={3}>
+              <Text color="gray">Material</Text>
+            </Grid>
+          </Grid.Container>
+        </div>
+        <div className="-mt-16">
+          <Grid.Container gap={4} justify="space-between">
+            <Grid xs={12} lg={6}>
+              <Input clearable type="search" width="100%" />
+            </Grid>
+            <Grid xs={12} lg={3}>
+              <Dropdown>
+                <Dropdown.Button flat css={{ tt: "capitalize" }}>
+                  {selectedCategoryValue ?? "Select Category"}
+                </Dropdown.Button>
+                <Dropdown.Menu
+                  aria-label="category-selection"
+                  selectionMode="single"
+                  selectedKeys={selectedCategory}
+                  onSelectionChange={setSelectedCategory}
+                >
+                  {categories.map((category) => (
+                    <Dropdown.Item key={category}>{category}</Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            </Grid>
+            <Grid xs={12} lg={3}>
+              <MaterialDropdown />
+            </Grid>
+          </Grid.Container>
+        </div>
+        <div className="mt-3">
+          <Grid.Container gap={2} justify="center">
+            {filteredPackages.length != 0
+              ? filteredPackages.map((pckg) => (
+                  <Grid key={pckg.id} xs={12} sm={4} lg={3}>
+                    <PackageCard
+                      key={pckg.id}
+                      pckg={{
+                        id: pckg.id,
+                        name: pckg.name,
+                        description: pckg.description,
+                        price: pckg.price,
+                        size: pckg.size,
+                        capacity: pckg.capacity,
+                        category: pckg.category,
+                        materials: pckg.materials,
+                      }}
+                    />
+                  </Grid>
+                ))
+              : "No match found."}
+          </Grid.Container>
+        </div>
+      </>
+  
   );
 }
 
--- src/components/dropdown/materialDropdown.tsx
+++ src/components/dropdown/materialDropdown.tsx
@@ -1,11 +1,24 @@
 import { Dropdown } from "@nextui-org/react";
 import { materials } from "../../types/enums";
+import { useState, useMemo } from "react";
+import { Selection } from "@react-types/shared";
 
 export default function MaterialDropdown() {
+  const [selected, setSelected] = useState<Selection>(new Set(["All"]));
+
+  const selectedValue = useMemo(() => Array.from(selected), [selected]);
+
   return (
     <Dropdown>
-      <Dropdown.Button flat>Select Material</Dropdown.Button>
-      <Dropdown.Menu aria-label="material-selection">
+      <Dropdown.Button flat css={{ tt: "capitalize" }}>
+        {selectedValue ?? "Select Material"}
+      </Dropdown.Button>
+      <Dropdown.Menu
+        aria-label="material-selection"
+        selectionMode="single"
+        selectedKeys={selected}
+        onSelectionChange={setSelected}
+      >
         {materials.map((material) => (
           <Dropdown.Item key={material}>{material}</Dropdown.Item>
         ))}
